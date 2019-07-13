4364b4ae422a72abefc9db6d53fbad17c9280d66
Peter Pike <pikepeter@gmail.com>
2019-07-13 16:26:03 +0800

    Update nav.blade.php

--- resources/views/layouts/partials/nav.blade.php
+++ resources/views/layouts/partials/nav.blade.php
@@ -1,7 +1,7 @@
 <nav class="sticky bg-gray-400 shadow py-2">
         <div class="flex items-center container mx-auto px-6 md:px-0">
             <div>
-                <img class="" src="{{URL::asset('images/headerlogo-300x65.png')}}" width='250px'  alt="Logo Pic">
+              <a href="http://manza.org">  <img class="" src="{{URL::asset('images/headerlogo-300x65.png')}}" width='250px'  alt="Logo Pic"></a>
             </div>
             <div style='color:#8C2E51' class="flex-1 font-muli flex flex-col justify-end items-center">
                 <h1 class="font-bold text-4xl" >Malaysian, Australian and New Zealand Association</h1>
