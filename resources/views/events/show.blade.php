7a4a7ea296bc31bed1d8001714437a0da4946583
Peter Pike <pikepeter@gmail.com>
2019-07-16 21:03:04 +0800

    Image on event

--- resources/views/events/show.blade.php
+++ resources/views/events/show.blade.php
@@ -8,30 +8,41 @@
 
 <div  class="container mx-auto">
     <div class="card mt-6 lg:w-3/5 mx-auto  rounded shadow">
-        <div class="bg-blue-100">
+ 
+        <div class="  ">
             <h1 class="text-3xl font-semibold mt-2 pb-4 text-center">
                 {{ $event->title }}
             </h1>
         </div>
-        <div class="text-lg bg-blue-100  ">
-            <div class="flex mb-2 ">
-              <div class="flex-1 pl-4 ">{{ $event->description }}</div>  
+        <div class="text-lg  ">
+            <div class=" flex">
+              <div class="">
+                @if(isset($event->featured_img))
+                   <div class=" max-w-md card mx-auto">
+                             <img class="w-full object-cover object-centre" src="{{URL::asset( $event->featured_img)}}">  
+                    </div>
+                @endif 
+              </div>
+                <div class="flex mb-2 ">
+                  <div class="flex-1 pl-4 ">{{ $event->description }}</div>  
+                </div>
             </div>
             <div class="flex mb-2">
-              <div class="w-1/6  text-right font-semibold ">Venue: </div>
-              <div class="ml-4">MANZA House, 38 Bukit Pantai</div>  
+              <div class="w-1/6 text-right font-semibold ">Venue: </div>
+              <div class="ml-4">SAO NAM BUKIT BINTANG
             </div>
+          </div>
             <div class="flex mb-2">
               <div class="w-1/6  text-right font-semibold ">Date: </div>
               <div class="ml-4">{{ $event->date_of_event }}</div>
             </div>
-            <div class="flex mb-2">  
+            <div class="flex mb-2 ">  
               <div class="w-1/6  text-right font-semibold ">From: </div>
-              <div class="ml-4">2pm</div>  
+              <div class="ml-4">7pm</div>  
               <div class="ml-1  text-left font-semibold ">-</div>
-              <div class="ml-2">7pm</div>  
+              <div class="ml-2">till late</div>  
             </div>
-            <div class="w-1/2 pl-4 text-left  font-semibold ">Ticket Prices: </div>
+            <div class="w-full pt-4 pl-4 mb-4 text-left  font-semibold border-t-2 ">Ticket Prices: </div>
             <div class=" ml-12 flex items-center">
                 <div class="bg-gray-400 w-1/2 py-2 px-6 border-b border-r border-grey-light font-semibold">
                   Ticket Type</div>
