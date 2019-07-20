06c8b5a867390c7e6e8fdd4f1451003bb7030eec
Peter Pike <pikepeter@gmail.com>
2019-07-20 17:24:12 +0800

    Create show.blade.php

---
+++ resources/views/memberships/show.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.app')
+
+@section('title', 'Read Message')
+
+@section('content')
+
+@include('layouts.partials.pageheader')
+
+<div  class="container mx-auto">
+    <div class=" mt-6 lg:w-4/5 lg:mx-auto ">
+        <div class="">
+            <h1 class="text-2xl text-gray-700 font-bold pb-4 text-center">
+                Membership No. {{ $membership->memb_no }} / <span class="uppercase">{{ $membership->surname }}</span>
+            </h1>
+        </div>
+            @include('memberships.form')
+        <div class="flex-1 text-sm ml-4 py-4">
+            <p><a class="no-underline hover:font-semibold"  href="{{ $url = '/membership' }}" ><i class="fas fa-backward"></i> Back</a></p>
+        </div>
+    </div>
+</div>
+
+
+@endsection
