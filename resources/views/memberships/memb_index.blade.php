540cd4347c202b176d6c0b7b3fe2bd2ec21847b1
Peter Pike <pikepeter@gmail.com>
2019-07-21 16:19:59 +0800

    Update memb_index.blade.php

--- resources/views/memberships/memb_index.blade.php
+++ resources/views/memberships/memb_index.blade.php
@@ -23,7 +23,7 @@
                 @foreach($members as $member)
                 <tr class="hover:bg-grey-lighter">
                   <td class="py-4 px-6 border-b border-r border-grey-light">{{ $member->formatted_date_joined }}</td>
-                  <td class="py-4 px-6 border-b border-r border-grey-light text-center">{{ $member->fullname }}</td>
+                  <td class="py-4 px-6 border-b border-r border-grey-light ">{{ $member->fullname }}</td>
                   <td class="py-4 px-6 border-b border-r border-grey-light text-center">{{ $member->nationality }}</td>
                   <td class="py-4 px-6 border-b border-r border-grey-light text-center">{{ $member->mobile }}</td>
                   <td class="py-4 px-6 border-b border-r border-grey-light text-center">{{ $member->email }}</td>
