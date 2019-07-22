38416e42fdac8190ed1bf8c73a3525d174de9049
Peter Pike <pikepeter@gmail.com>
2019-07-22 22:07:57 +0800

    Update index.blade.php

--- resources/views/members/index.blade.php
+++ resources/views/members/index.blade.php
@@ -20,27 +20,27 @@
                 </div>
                 <div class="flex flex-col ">
                     <div class=" mx-auto">
-                  <div class="bg-white shadow-md rounded my-6">
+                  <div class="bg-white  rounded my-6">
                     <table class="text-left w-full border-collapse"> <!--Border collapse doesn't work on this site yet but it's available in newer tailwind versions -->
                       <thead>
                         <tr>
-                          <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t border-b border-r  border-grey-light">Date Joined</th>
-                          <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t border-b border-r  border-grey-light">Member No.</th>
-                          <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t border-b border-r  border-grey-light">Name</th>
-                          <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t border-b border-r  border-grey-light">Nationality</th>
-                          <th class="py-4 px-6 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t  border-b border-r  border-grey-light">Mobile</th>
-                          <th class="py-4 bg-grey-lightest font-bold uppercase text-sm text-center text-grey-dark border-t  border-b border-r  border-grey-light">Email</th>
+                          <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t border-b border-r  border-grey-light">Date Joined</th>
+                          <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t border-b border-r  border-grey-light">Member No.</th>
+                          <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t border-b border-r  border-grey-light">Name</th>
+                          <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t border-b border-r  border-grey-light">Nationality</th>
+                          <th class="py-2 px-4 bg-grey-lightest font-bold uppercase text-sm text-grey-dark border-t  border-b border-r  border-grey-light">Mobile</th>
+                          <th class="py-2 bg-grey-lightest font-bold uppercase text-sm text-center text-grey-dark border-t  border-b border-r  border-grey-light">Email</th>
                         </tr>
                       </thead
                       <tbody>
                         @foreach($members as $member)
                         <tr class="hover:bg-grey-lighter">
-                          <td class="py-4 px-6 border-b border-r border-grey-light">{{ $member->formatted_date_joined }}</td>
-                          <td class="py-4 px-6 border-b border-r border-grey-light"><a href='/membership/{{ $member->membership->id }}'>{{ $member->membership->memb_no }}</a></td>
-                          <td class="py-4 px-6 border-b border-r border-grey-light text-center">{{ $member->fullname }}</td>
-                          <td class="py-4 px-6 border-b border-r border-grey-light text-center">{{ $member->nationality }}</td>
-                          <td class="py-4 px-6 border-b border-r border-grey-light text-center">{{ $member->mobile }}</td>
-                          <td class="py-4 px-6 border-b border-r border-grey-light text-center">{{ $member->email }}</td>
+                          <td class="py-4 px-4 border-b border-r border-grey-light">{{ $member->formatted_date_joined }}</td>
+                          <td class="py-4 px-4 border-b border-r border-grey-light"><a href='/membership/{{ $member->membership->id }}'>{{ $member->membership->memb_no }}</a></td>
+                          <td class="py-4 px-4 border-b border-r border-grey-light text-center">{{ $member->fullname }}</td>
+                          <td class="py-4 px-4 border-b border-r border-grey-light text-center">{{ $member->nationality }}</td>
+                          <td class="py-4 px-4 border-b border-r border-grey-light text-center">{{ $member->mobile }}</td>
+                          <td class="py-4 px-4 border-b border-r border-grey-light text-center">{{ $member->email }}</td>
                           <td class=" border-b border-r  border-grey-light">
 
                           </td>
@@ -50,6 +50,7 @@
                       </tbody>
                     </table>
                   </div>
+                  <p>{{ $members->count() }}</p>
                 </div>
                 </div>
             </div>
