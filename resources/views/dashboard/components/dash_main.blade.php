193684bac3b92a80495f585213615342681c381d
Peter Pike <pikepeter@gmail.com>
2019-07-25 21:45:06 +0800

    Update dash_main.blade.php

--- resources/views/dashboard/components/dash_main.blade.php
+++ resources/views/dashboard/components/dash_main.blade.php
@@ -7,23 +7,24 @@
                         <img class=" rounded-lg object-cover object-centre w-full" src={{ $event->featured_img }} alt="<{{ $event->title }}>">
                     @endif
                 </div>
-                    <div class=" card mt-4 text-center">
-                        <h1 class="text-xl text-indigo-900 font-semibold p-2">{{ $event->title }}</h1>
+                <div class="flex-1"></div> {{-- This is a filler  --}}
+                <div class=" card mt-4 text-center">
+                    <h1 class="text-xl text-indigo-900 font-semibold p-2">{{ $event->title }}</h1>
+                </div>
+                <div class="flex flex-col justify-between ">
+                    <div class="flex-1 h-auto ">
+                        <p class=" mt-4">{!! nl2br(substr($event->description,0,100))!!}
+                        <a class="text-blue-900 font-extrabold no-underline" href="{{ $url = action('EventController@show', ['id' => $event->id]) }}" >... more <i class="fas fa-angle-double-right"></i></a></p>
                     </div>
-                    <div class="flex flex-col justify-between ">
-                        <div class="flex-1 h-auto ">
-                            <p class=" mt-4">{!! nl2br(substr($event->description,0,100))!!}
-                            <a class="text-blue-900 font-extrabold no-underline" href="{{ $url = action('EventController@show', ['id' => $event->id]) }}" >... more <i class="fas fa-angle-double-right"></i></a></p>
-                        </div>
 
-                        <div class="">
-                           {{--  @include('dashboard.components._pricing')  --}}          
-                        </div>
-
-                        <div class="text-center mt-4 text-xl text-indigo-900 font-semibold">
-                            <p>Tickets Available</p>
-                        </div>
+                    <div class="">
+                       {{--  @include('dashboard.components._pricing')  --}}          
                     </div>
+
+                    <div class="text-center mt-4 text-xl text-indigo-900 font-semibold">
+                        <p>Tickets Available</p>
+                    </div>
+                </div>
             </div>
         @endforeach
 
