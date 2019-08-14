d39e2ce70ab6287c85c3102eed35dba0b66df71f
Peter Pike <pikepeter@gmail.com>
2019-08-14 20:03:04 +0800

    Create 2019_07_28_093505_create_bookings_table.php

---
+++ database/migrations/2019_07_28_093505_create_bookings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateBookingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('bookings', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->unsignedBigInteger('event_id');
+            $table->string('email');
+            $table->string('name');
+            $table->string('memb_no')->nullable();
+            $table->string('add_info')->nullable();
+            $table->timestamp('confirmed_at')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('bookings');
+    }
+}
