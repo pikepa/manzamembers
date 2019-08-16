beecab0d4b180b8cf1c704bb348bbcddf8360503
Peter Pike <pikepeter@gmail.com>
2019-08-16 17:07:44 +0800

    Update EventBookingsController.php

--- app/Http/Controllers/EventBookingsController.php
+++ app/Http/Controllers/EventBookingsController.php
@@ -19,7 +19,7 @@ class EventBookingsController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show','create']]);
+        $this->middleware('auth', ['except' => ['index', 'show','create','store']]);
     }
 
     private $paymentGateway;
