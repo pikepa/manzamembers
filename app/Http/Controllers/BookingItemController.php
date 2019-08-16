a355a5be3da38469271053a0c4affaaf544fc5be
Peter Pike <pikepeter@gmail.com>
2019-08-16 17:11:46 +0800

    Update BookingItemController.php
    Removing need to be logged in to book

--- app/Http/Controllers/BookingItemController.php
+++ app/Http/Controllers/BookingItemController.php
@@ -16,7 +16,7 @@ class BookingItemController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth', ['except' => ['index', 'show','store']]);
     }
 
     /**
