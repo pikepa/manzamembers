f5feb57fa9e8009c5fe7af218bba41f377e6f960
Peter Pike <pikepeter@gmail.com>
2019-08-16 17:13:39 +0800

    Update CheckoutController.php
    Finally working . This is v2, to be updated

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -3,66 +3,41 @@
 namespace App\Http\Controllers;
 use Session;
 use App\Booking;
+use Stripe;
 use App\BookingItem;
-use Cartalyst\Stripe\Stripe;
 use Illuminate\Http\Request;
 
 class CheckoutController extends Controller
 {
     public function precheckout()
     {
-        $booking=Booking::findOrFail(session::get('booking_id'));
-        $totalcost=BookingItem::cost();
-        $totaltickets=BookingItem::tickets();
-
-        $stripe= new Stripe(getenv('STRIPE_SECRET_KEY'));
-
-        $customer=$stripe->customers()->create([
-                'email' => $booking->email,
-            ]);
-        dd($customer);
-        $charge=$stripe->charges()->create([
-            'currency' => 'MYR',
-            'amount' => $totalcost/100,
-        ]);
-
-
-            dd($charge);
-             
-
-        // Set your secret key: remember to change this to your live secret key in production
-        // See your keys here: https://dashboard.stripe.com/account/apikeys
-        \Stripe\Stripe::setApiKey(getenv('STRIPE_SECRET_KEY'));
-
-        $charge = \Stripe\Charge::create([
-            'amount' => $totalcost,
-            'currency' => 'myr',
-            'source' => 'tok_visa',
-            'receipt_email' => 'pikepeter@gmail.com',
-        ]);
-            return view('stripe.checkout');    
-            dd($charge);
+      $booking=Booking::findOrFail(session::get('booking_id'));
+      $totalcost=BookingItem::cost()/100;
+      $totaltickets=BookingItem::tickets();
+               
+      return view('stripe.newcheckout', compact('totalcost'));    
              
 
     }
-public function stripecheckout()
-{
-    // Set your secret key: remember to change this to your live secret key in production
-// See your keys here: https://dashboard.stripe.com/account/apikeys
-\Stripe\Stripe::setApiKey('sk_test_3krh95EvbbFtt0XBNhzDEMab00yYW4d64O');
+    public function charge(Request $request)
+    {
+      $booking=Booking::findOrFail(session::get('booking_id'));
+      $totalcost=BookingItem::cost();
+      $totaltickets=BookingItem::tickets();
 
-        $session = \Stripe\Checkout\Session::create([
-          'payment_method_types' => ['card'],
-          'line_items' => [[
-            'name' => 'Event tickets',
-            'description' => 'Tickets for Event X',
-            'amount' => 430000,
-            'currency' => 'myr',
-            'quantity' => 7,
-          ]],
-          'success_url' => 'https://example.com/success',
-          'cancel_url' => 'https://example.com/cancel',
+        Stripe\Stripe::setApiKey(env('STRIPE_SECRET_KEY'));
+
+        $charged= Stripe\Charge::create ([
+                "amount" => $totalcost,
+                "currency" => "myr",
+                "source" => $request->stripeToken,
+                "description" => 'Booking for '.$totaltickets .' @ RM '.$totalcost/100
         ]);
-}
+  
+        Session::flash('success', 'Payment successful!');
+         dd($charged); 
+        return back();
+
+    }
 
 }
