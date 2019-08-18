e9ebb7f5df9d1ba68979e3e35f6ef0940b7e1c49
Peter Pike <pikepeter@gmail.com>
2019-08-18 18:21:25 +0800

    Update create.blade.php

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -7,7 +7,7 @@
 @include('layouts.partials.pageheader')
 
 <div  class="container mx-auto">
-    <div class="lg:w-1/2 lg:mx-auto card p-6 md:py-6 md:px-16 rounded shadow">
+    <div class="lg:w-3/5 lg:mx-auto card p-6 md:py-6 md:px-16 rounded shadow">
         <h1 class="text-2xl font-normal mb-4 text-center">
             New Event
         </h1>
