f6fd46abc98a88da164f715611b48495ce69ca89
Peter Pike <pikepeter@gmail.com>
2019-08-18 15:08:23 +0800

    images and success page

--- resources/views/stripe/success.blade.php
+++ resources/views/stripe/success.blade.php
@@ -1,13 +1,13 @@
 @extends('layouts.app')
 
-@section('title', 'Checkout')
+@section('title', 'Success')
 
 @section('content')
 
 @include('layouts.partials.pageheader')
 
 <div  class="container mx-auto">
-    <div class="flex flex-col items-center lg:w-1/2 lg:mx-auto card p-6 md:py-6 md:px-16 rounded shadow">
+    <div class="flex flex-col items-center lg:w-1/2 lg:mx-auto card mt-6 p-6 md:py-6 md:px-16 rounded shadow">
         <div class=" mb-4 align-center ">
             <img class="  " src="{{URL::asset( '/images/tick.png')}}" height=100px>
         </div>
@@ -19,6 +19,7 @@
        </h2>
         <div class="text-center">
             <a class="font-bold text-xl underline"href="{{$booking->receipt_url}}" target="_blank">Here is your receipt</a>
+            <p>Please print or save it as to your pc or phone as an additional safeguard.</p>
         </div>
 
    </div>
