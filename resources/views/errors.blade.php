3a665e02344ade91820b7ea2cd9f189191b0bce4
Peter Pike <pikepeter@gmail.com>
2019-08-19 20:28:09 +0800

    edit to error processing in forms

--- resources/views/errors.blade.php
+++ resources/views/errors.blade.php
@@ -1,9 +1,7 @@
 @if ($errors->{ $bag ?? 'default' }->any())
-<div class="mb-4 rounded p-2 text-red-700 font-bold border-red-700 border-solid border-2">
-        <ul class="field list-reset">
+    <ul class="w-2/3 mx-auto mb-4 rounded p-2 text-center text-red-700 font-bold border-red-700 border-solid border-2">
         @foreach ($errors->{ $bag ?? 'default' }->all() as $error)
             <li class="text-sm text-red">{{ $error }}</li>
         @endforeach
     </ul>
-</div>
 @endif
