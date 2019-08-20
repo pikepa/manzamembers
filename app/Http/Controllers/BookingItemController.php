8c71748afe72c74fbb90b32c98a5953d62b3b1b1
Peter Pike <pikepeter@gmail.com>
2019-08-20 22:11:40 +0800

    Update BookingItemController.php

--- app/Http/Controllers/BookingItemController.php
+++ app/Http/Controllers/BookingItemController.php
@@ -16,7 +16,7 @@ class BookingItemController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show','store']]);
+        $this->middleware('auth', ['except' => ['index', 'show','store','destroy']]);
     }
 
     /**
