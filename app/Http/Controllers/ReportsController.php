fb2577d73140215253a6c8006bef90e05a0fc44c
Peter Pike <pikepeter@gmail.com>
2019-08-22 17:22:19 +0800

    Update ReportsController.php

--- app/Http/Controllers/ReportsController.php
+++ app/Http/Controllers/ReportsController.php
@@ -37,7 +37,7 @@ class ReportsController extends Controller
     public function receipt_listing()
     {
         $receipts = Receipt::with('membership','term')
-        ->orderBy('mship_term_id','asc')->get();
+        ->orderBy('mship_term_id','desc')->get();
         return view('receipts.receipt_index', compact('receipts'));
                 
     } 
