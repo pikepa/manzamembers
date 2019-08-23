04787675b3d61faf8155182279ff7914b676fabd
Peter Pike <pikepeter@gmail.com>
2019-08-23 22:06:54 +0800

    Update services.php

--- config/services.php
+++ config/services.php
@@ -41,8 +41,8 @@ return [
 
     'stripe' => [
         'model' => App\User::class,
-        'key' => env('STRIPE_KEY'),
-        'secret' => env('STRIPE_SECRET'),
+        'key' => env('STRIPE_PUBLIC_KEY'),
+        'secret' => env('STRIPE_SECRET_KEY'),
         'webhook' => [
             'secret' => env('STRIPE_WEBHOOK_SECRET'),
             'tolerance' => env('STRIPE_WEBHOOK_TOLERANCE', 300),
