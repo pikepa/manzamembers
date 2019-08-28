0d50e86ac039c10423ef0323e3984716ad4ed6f2
Peter Pike <pikepeter@gmail.com>
2019-08-28 21:05:29 +0800

    Update show.blade.php

--- resources/views/booking_items/show.blade.php
+++ resources/views/booking_items/show.blade.php
@@ -6,38 +6,42 @@
 
 @include('layouts.partials.pageheader')
 
-<div  class="container mx-auto md:w-3/5">
-    <div class=" mx-auto card rounded shadow">
+<div  class="container mx-auto md:w-1/2">
+    <div class=" mx-auto mt-4 card rounded shadow">
 
         @include('messages')
 
         <h1 class="text-2xl font-normal mb-2 text-center">
             {{ $eventbooking->title }} - {{ $eventbooking->date_of_event }}
         </h1>
-          <div class="mt-2 md:w-2/3 mx-auto flex items-center">
-              <div class="bg-gray-400  py-2 px-4 border border-grey-light font-semibold">
+          <div class="mt-2  md:w-2/3 mx-auto flex items-center">
+              <div class="bg-gray-400 w-1/6 md:w-1/6 py-2 px-2 text-center border border-grey-light font-semibold">
                 Qty
               </div>                    
-              <div class="flex-1 bg-gray-400  py-2 px-4 border border-grey-light font-semibold">
-                Ordered Tickets
+              <div class="flex-1 bg-gray-400 text-center py-2 px-2 border border-grey-light font-semibold">
+                Orders
               </div>
-              <div class="bg-gray-400 text-center py-2 px-4 border border-grey-light font-semibold">Cost</div>
+              <div class="bg-gray-400 w-2/5 md:w-1/4 text-center py-2 px-4 border border-grey-light font-semibold">Cost</div>
           </div>
                 @foreach($orders as $order)
                    <div class="md:w-2/3 mx-auto  ">
                         <div class="flex border border-grey-light items-center">
-                            <div class=" py-2 px-4 text-center ">{{ $order->qty}}</div>
-                            <div class=" py-2 px-4  ">
-                                @if($order->priceitems['memb'] === 0)Non-Member - @else Member - @endif 
+                            <div class="w-1/6 py-2 px-4 text-center border border-grey-light ">{{ $order->qty}}</div>
+                            <div class="flex-1 py-2 px-4 border border-grey-light ">
+                                @if($order->priceitems['memb'] === 0)Non-Member - 
+                                @elseif($order->priceitems['memb'] === 0)Member - 
+                                @else  
+                                @endif 
                                 {{ $order->category['category']}}
                             </div>
-                            <div class="text-right py-2 px-6 border border-grey-light ">{{ $order->display_cost }}</div>
+                            <div class="text-right w-2/5 md:w-1/4 py-2 px-2 border border-grey-light ">{{ $order->display_cost }}</div>
                             
                         </div>
                     @include('layouts.partials.icons._delete',['variable' => $order->path()])
 
                     </div>
-                 @endforeach
+                @endforeach
+
                 <div class="mt-8 md:w-2/3 mx-auto ">
                     <div class="control">
                         <a href="/checkout" class="btn btn-manza is-link mr-2">Checkout</a>
@@ -49,6 +53,7 @@
         <div class=" mx-auto">
         </div>
         <div class=" pb-4 mx-auto flex flex-col md:flex-row justify-center card">
+          @if(session::get('memb_required') !== "NO")
             <div class=""> 
                 <form 
                         method="POST"
@@ -69,6 +74,19 @@
                     ]) 
                 </form>
             </div>
+            @elseif(session::get('memb_required') == "NO")
+            <div> 
+                <form 
+                        method="POST"
+                        action="/bookingitems"
+                >
+                    @include ('booking_items.other_form', [
+                        'buttonText' => 'Add'
+                    ]) 
+                </form>
+            </div>
+
+            @endif
         </div>     
     </div>
 
