d97afeb217002b11329e1427902c56dae6584a2f
Peter Pike <pikepeter@gmail.com>
2019-09-01 06:22:46 +0800

    Add Ignition Errors to Dev

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -7,6 +7,14 @@ use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 
 class Handler extends ExceptionHandler
 {
+    protected function whoopsHandler()
+        {
+            try {
+                return app(\Whoops\Handler\HandlerInterface::class);
+            } catch (\Illuminate\Contracts\Container\BindingResolutionException $e) {
+                return (new \Illuminate\Foundation\Exceptions\WhoopsHandler)->forDebug();
+            }
+        }
     /**
      * A list of the exception types that are not reported.
      *
