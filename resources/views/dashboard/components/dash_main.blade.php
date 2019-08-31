ff62f5a40e23921c6668d9503705312ca9b33f25
Peter Pike <pikepeter@gmail.com>
2019-08-31 20:18:58 +0800

    Update dash_main.blade.php

--- resources/views/dashboard/components/dash_main.blade.php
+++ resources/views/dashboard/components/dash_main.blade.php
@@ -3,16 +3,16 @@
             <div class="flex flex-col md:w-1/4 content-between card mb-2 ml-1">
                 <div class="h-10px mx-auto text-center">
                     @if(isset($event->featured_img))
-                        <img class=" rounded-lg object-cover object-centre w-full" src={{ $event->featured_img }} alt="<{{ $event->title }}>">
+                        <img style='height:300px' class=" rounded-lg object-cover object-centre w-full" src={{ $event->featured_img }} alt="<{{ $event->title }}>">
                     @endif
                 </div>
-                <div class="flex-1"></div> {{-- This is a filler  --}}
-                <div class=" card text-center">
-                    <h1 class="text-xl text-indigo-900 font-semibold p-2">{{ $event->title }}</h1>
+                <div class=""></div> {{-- This is a filler  --}}
+                <div class="h-16  mt-1 pb-4 card border  border-gray-500 text-center">
+                    <h1 class="text-xl text-indigo-900 font-semibold pt-2 ">{{ $event->title }}</h1>
                 </div>
                 <div class="flex flex-col justify-between ">
                     <div class="flex-1 h-auto ">
-                        <p class=" mt-4">{!! nl2br(substr($event->description,0,100))!!}
+                        <p class="mt-4">{!! nl2br(substr($event->description,0,100))!!}
                         <a class="text-blue-900 font-extrabold no-underline" href="{{ $url = action('EventController@show', ['id' => $event->id]) }}" > ... see more <i class="fas fa-angle-double-right"></i></a></p>
                     </div>
 
@@ -21,8 +21,7 @@
                     </div>
                     @if($event->bookings_only === "Bookings Only" and $event->ispublished())
                         <div class="text-center mt-4 text-lg text-pink-600 font-extrabold ">
-                            <p>Bookings Open</p>
-                        </div>
+                   <a href="{{ $url = action('EventController@show', ['id' => $event->id]) }}" >Bookings Open</a>                        </div>
                     @endif
                     @if($event->bookings_only === "Booking & Tickets" and $event->ispublished())
                         <div class="text-center mt-4 text-lg text-pink-600 font-extrabold">
