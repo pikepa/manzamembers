a6c6513d87b5b63cfad966f7790a0e8d480a6c4d
Peter Pike <pikepeter@gmail.com>
2019-09-07 06:42:14 +0800

    Addition of Sold Out Status

--- resources/views/dashboard/components/dash_main.blade.php
+++ resources/views/dashboard/components/dash_main.blade.php
@@ -19,20 +19,7 @@
                     <div class="">
                        {{--  @include('dashboard.components._pricing')  --}}          
                     </div>
-                    @if($event->bookings_only === "Bookings Only" and $event->ispublished())
-                        <div class="text-center mt-4 text-lg text-pink-600 font-extrabold ">
-                   <a href="{{ $url = action('EventController@show', ['id' => $event->id]) }}" >Register Now</a>                        </div>
-                    @endif
-                    @if($event->bookings_only === "Booking & Tickets" and $event->ispublished())
-                        <div class="text-center mt-4 text-lg text-pink-600 font-extrabold">
-                        <a href="{{ $url = action('EventController@show', ['id' => $event->id]) }}" >Get Your Tickets Now</a>
-                        </div>
-                    @endif
-                    @if( $event->isnotpublished())
-                        <div class="text-center no_underline mt-4 text-lg text-indigo-900 font-extrabold">
-                            <p>Bookings Opening Soon</p>
-                        </div>
-                    @endif
+                    @include('dashboard.components._dash_main_status')
                 </div>
             </div>
         @endforeach
