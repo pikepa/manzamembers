80bd295e7a4d32a56487844be96937094135b28a
Peter Pike <pikepeter@gmail.com>
2019-09-08 13:22:30 +0800

    Update EventController.php

--- app/Http/Controllers/EventController.php
+++ app/Http/Controllers/EventController.php
@@ -18,7 +18,7 @@ class EventController extends Controller
      */
     public function __construct()
     {
-        $this->middleware('auth', ['except' => ['index', 'show']]);
+        $this->middleware('auth', ['except' => [ 'show']]);
     }
 
     /**
