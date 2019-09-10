78cc2f78493eee2216f143399c792c618d84089c
Peter Pike <pikepeter@gmail.com>
2019-09-10 20:28:09 +0800

    Update create.blade.php

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -9,14 +9,14 @@
 <div  class="container mx-auto">
     <div class="lg:w-1/2 lg:mx-auto card p-6 md:py-6 md:px-16 rounded shadow">
         <h1 class="text-2xl font-normal mb-4 text-center">
-            New User
+            Invite New User
         </h1>
         <form
             method="POST"
             action="/users"
         >
             @include ('users.form', [
-                'buttonText' => 'Save'
+                'buttonText' => 'Invite'
             ])
         </form>
     </div>
