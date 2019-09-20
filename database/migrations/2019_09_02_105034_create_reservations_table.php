8b13f64e3c4be38cc116ca9c427f28a425e41c81
Peter Pike <pikepeter@gmail.com>
2019-09-20 17:14:53 +0800

    Make mobile nullable

--- database/migrations/2019_09_02_105034_create_reservations_table.php
+++ database/migrations/2019_09_02_105034_create_reservations_table.php
@@ -17,7 +17,7 @@ class CreateReservationsTable extends Migration
             $table->bigIncrements('id');
             $table->bigInteger('event_id');
             $table->text('name');
-            $table->text('mobile');
+            $table->text('mobile')->nullable();
             $table->text('email');
             $table->integer('res_qty');
             $table->timestamps();
