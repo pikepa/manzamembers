ddcc4f50abe618f25d1121b1165db781880efc26
Peter Pike <pikepeter@gmail.com>
2019-10-04 14:17:58 +0800

    Update create.blade.php

--- resources/views/reservations/create.blade.php
+++ resources/views/reservations/create.blade.php
@@ -7,7 +7,7 @@
 @include('layouts.partials.pageheader')
 
 <div  class="container mx-auto">
-    <div class="lg:w-1/2 lg:mx-auto bg-card p-6 md:py-12 md:px-16 rounded shadow">
+    <div class="lg:w-1/2 lg:mx-auto card p-6 md:py-12 md:px-16 rounded shadow">
         <h1 class="text-2xl font-normal mb-10 text-center">
             Make a Reservation
         </h1>
