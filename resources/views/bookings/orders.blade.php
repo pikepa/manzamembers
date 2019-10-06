ec41b1d7d044265f44d2b1bf97a5877b7ed31617
Peter Pike <pikepeter@gmail.com>
2019-10-06 20:03:34 +0800

    Update orders.blade.php

--- resources/views/bookings/orders.blade.php
+++ resources/views/bookings/orders.blade.php
@@ -6,7 +6,7 @@
 
 @include('layouts.partials.pageheader')
 
-<div  class="container mx-auto md:w-2/3">
+<div  class="container mx-auto ">
     <div class=" mx-auto mt-10 card rounded shadow">
 
         @include('messages')
@@ -17,12 +17,15 @@
         @foreach($orders as $priceitem => $orders_list)
           <div class="mt-2 md:w-4/5  mx-auto flex justify-between items-center">
                 <div class="bg-gray-400 w-1/6 md:w-1/6 py-2 px-2 text-center border border-grey-light font-semibold">
+                    Name
+                </div>
+                <div class="bg-gray-400 w-12  py-2 px-2 text-center border border-grey-light font-semibold">
                     Qty
                 </div>                    
                 <div class="flex-1 bg-gray-400 text-center py-2 px-2 border border-grey-light font-semibold">
                     Orders
                 </div>
-                <div class="bg-gray-400 w-2/5 md:w-1/5 md:mr-1 text-center py-2 px-2 border border-grey-light font-semibold">
+                <div class="bg-gray-400 w-2/5 md:w-1/5 text-center py-2 px-2 border border-grey-light font-semibold">
                   Cost
                 </div>
                 <div class="bg-gray-400 w-2/5 md:w-1/5 md:mr-1 text-center py-2 px-2 border border-grey-light font-semibold">
@@ -31,7 +34,8 @@
           </div>
                 @foreach($orders_list as $order)
                         <div class="md:w-4/5 mx-auto flex items-center">
-                            <div class="w-1/6 py-2 px-4 text-center border border-grey-light ">{{ $order->qty}}</div>
+                            <div class="w-1/6 py-2 px-4 text-center border border-grey-light ">{{ $order->booking->name}}</div>
+                            <div class="w-12 py-2 px-4 text-center border border-grey-light ">{{ $order->qty}}</div>
                             <div class="flex-1 py-2 px-4 border border-grey-light ">
                                 @if($order->priceitems['memb'] === 0)Non-Member - 
                                 @elseif($order->priceitems['memb'] === 0)Member - 
@@ -39,14 +43,15 @@
                                 @endif 
                                 {{ $order->category['category']}}
                             </div>
-                            <div class="md:mr-1 text-right w-2/5 md:w-1/5 py-2 px-2 border border-grey-light ">{{ $order->display_cost }}</div>
+                            <div class=" text-right w-2/5 md:w-1/5 py-2 px-2 border border-grey-light ">{{ $order->display_cost }}</div>
                             @if($order->booking->confirmed_at !== null)
                                 <div class="md:mr-1 text-right w-2/5 md:w-1/5 py-2 px-2 border border-grey-light ">{{ $order->booking->confirmed_at->format('d-m-Y') }}</div>
                             @elseif($order->booking->confirmed_at == null)
-                                <div class="md:mr-1 text-right w-2/5 md:w-1/5 py-2 px-2 border border-grey-light ">Not Paid</div>
+                                <div class="md:mr-1 text-center w-2/5 md:w-1/5 py-2 px-2 border border-grey-light text-red-600 font-extrabold uppercase ">Not Paid</div>
                             @endif
                         </div>
                 @endforeach
+                <br>
                 @endforeach
                 @if($cartreceipts->count() !==0 )
                          <h1 class="mt-12 text-2xl font-normal mb-6 text-center">
