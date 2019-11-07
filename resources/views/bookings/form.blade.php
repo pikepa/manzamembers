a281d107c3da9402c63ca8ae3b9acbd0616bde5b
Peter Pike <pikepeter@gmail.com>
2019-11-08 07:02:26 +0800

    Update form.blade.php

--- resources/views/bookings/form.blade.php
+++ resources/views/bookings/form.blade.php
@@ -24,8 +24,24 @@
             </label>
         </div>
         
- 
-
+         <div class="w-1/2  block mb-6">
+              <span class=" font-bold text-gray-800">Select Organisation</span>
+              <div class="mt-1">
+                <div class=" flex flex-wrap ">
+                  <div class="inline-block relative w-full">
+                    <select name="add_info" class="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+                      <option value=''>Select your Organisation</option>
+                      <option value='MANZA'>MANZA</option>
+                      <option value='MNZCC'>MNZCC</option>
+                      <option value='MABC'>MABC</option>
+                    </select>
+                    <div class="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+                      <svg class="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+                    </div>
+                  </div>
+                </div>
+              </div>
+{{-- 
         <div class="flex flex-col md:flex-row justify-between">
             <div class="md:w-1/2 field mb-6">
                 <label class="block">
@@ -36,7 +52,9 @@
                             value="{{old('memb_no', $eventbooking->booking['memb_no'])}}"> 
                 </label>
             </div>
-  {{--  top of ticket section          
+
+
+    top of ticket section          
             <div class="md:w-1/2 field mb-6">
                 <label class="block ">
                 @if($eventbooking->add_info !== null)
