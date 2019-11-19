f35a83ca65215c2e9fb6620324339d9223217f06
Peter <pikepa@users.noreply.github.com>
2019-11-19 08:48:29 +0800

    Update index.blade.php
    Trying to force Cache

--- resources/views/bookings/index.blade.php
+++ resources/views/bookings/index.blade.php
@@ -17,6 +17,7 @@
                 <div class="text-center">
                   @include('messages')
                     <h1 class="font-bold text-3xl m-2 ">{{$event->title}} Bookings</h1>
+                    <h1 class="font-bold text-3xl m-2  >Today is 19th November 2019</h1>
                     <p><a href='{{ $event->id }}/orders'>View Orders</a></p>
                 </div>
                 <div class="flex flex-col ">
